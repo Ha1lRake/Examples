065341bfa17e9ed96e99fae33c66f2f6041ce12b
Ilya <ilya-osipchuk@inbox.ru>
2023-03-03 17:55:09 +0300

    Example005

---
+++ Example005_ifElse/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите имя пользователя:");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "русик") 
+{
+    Console.WriteLine("Ура, это же Русик");
+}
+
+else
+{
+    Console.Write ("Привет,");
+    Console.WriteLine(username);   
+}
+    
